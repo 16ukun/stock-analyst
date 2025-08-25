d53a1ee585613fef15ddc56768fa63671ddb0102
Jeremiah.Osidele <Jeremiah.Osidele@fdmgroup.com>
2025-08-25 13:56:11 +0100

    feat: initialise project dependencies and configure environmentdev and prod

---
+++ stockAnalystMvp/src/main/java/com/ijosidele/stockAnalystMvp/StockAnalystMvpApplication.java
@@ -0,0 +1,13 @@
+package com.ijosidele.stockAnalystMvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockAnalystMvpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockAnalystMvpApplication.class, args);
+	}
+
+}
---
+++ stockAnalystMvp/src/test/java/com/ijosidele/stockAnalystMvp/StockAnalystMvpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijosidele.stockAnalystMvp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAnalystMvpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
