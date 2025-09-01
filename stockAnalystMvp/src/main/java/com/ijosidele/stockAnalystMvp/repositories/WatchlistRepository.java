860f377ad9b694358a9ed35b3c823b8cb17fab40
16ukun <136078903+16ukun@users.noreply.github.com>
2025-09-01 23:46:38 +0100

    feat: initialise JPA repositories (#3)
    Co-authored-by: Jeremiah.Osidele <Jeremiahosidele@gmail.com>

---
+++ stockAnalystMvp/src/main/java/com/ijosidele/stockAnalystMvp/repositories/WatchlistRepository.java
@@ -0,0 +1,13 @@
+package com.ijosidele.stockAnalystMvp.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ijosidele.stockAnalystMvp.models.Watchlist;
+
+public interface WatchlistRepository extends JpaRepository<Watchlist, Long> {
+	  @EntityGraph(attributePaths = {"items"})
+	  Optional<Watchlist> findByUser_Email(String email);
+}
